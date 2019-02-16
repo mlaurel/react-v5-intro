82bcaa09824fc49a2e7a956748899e5dbbee2908
Brian Holt <btholt@gmail.com>
2019-02-16 19:25:13 +1100

    introduce effects to populate dog dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { ANIMALS } from "petfinder-client";
+import pf, { ANIMALS } from "petfinder-client";
 import useDropdown from "./useDropdown";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("Seattle, WA");
   const [breeds, updateBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
+    petfinder.breed.list({ animal }).then(data => {
+      updateBreeds(
+        Array.isArray(data.petfinder.breeds.breed)
+          ? data.petfinder.breeds.breed
+          : [data.petfinder.breeds.breed]
+      );
+    }, console.error);
+  }, [animal]);
 
   return (
     <div className="search-params">
